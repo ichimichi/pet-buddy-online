345584464dc03d6dc6c827e47fb5b240666c6263
Laribok Syiemlieh <ichimichilaribok@gmail.com>
2020-11-04 14:37:02 +0530

    added sidebar drawer

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,44 +2,47 @@ import { AppBar, IconButton, Toolbar, Typography } from '@material-ui/core';
 import {
   Menu as MenuIcon,
   ExitToApp as ExitToAppIcon,
-  AddBox as AddBoxIcon,
 } from '@material-ui/icons';
-import React from 'react';
+import { tr } from 'date-fns/locale';
+import React, { useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { logout } from '../../Provider/AuthProvider';
+import { SideBarWithRouter } from './SideBar';
 import { useStyles } from './useStyles';
 
 export const NavBar = ({ history }) => {
   const classes = useStyles();
+  const [opened, setOpened] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setOpened(!opened);
+  };
+
   return (
-    <AppBar position="static" color="secondary">
-      <Toolbar>
-        <IconButton edge="start" color="inherit">
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" color="inherit">
-          Pet Buddy Online
-        </Typography>
-        <div className={classes.grow} />
-        <IconButton
-          color="inherit"
-          onClick={() => {
-            logout();
-            history.push('/signin');
-          }}
-        >
-          <ExitToAppIcon />
-        </IconButton>
-        <IconButton
-          color="inherit"
-          onClick={() => {
-            history.push('/itemReg');
-          }}
-        >
-          <AddBoxIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="static" color="secondary">
+        <SideBarWithRouter opened={opened} toggleDrawer={handleDrawerToggle} />
+
+        <Toolbar>
+          <IconButton edge="start" color="inherit" onClick={handleDrawerToggle}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" color="inherit">
+            Pet Buddy Online
+          </Typography>
+          <div className={classes.grow} />
+          <IconButton
+            color="inherit"
+            onClick={() => {
+              logout();
+              history.push('/signin');
+            }}
+          >
+            <ExitToAppIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
   );
 };
 
---
+++ src/Components/NavBar/SideBar.js
@@ -0,0 +1,51 @@
+import {
+  Box,
+  Divider,
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  Typography,
+} from '@material-ui/core';
+import React from 'react';
+import { AddBox as AddBoxIcon, Home as HomeIcon } from '@material-ui/icons';
+import { withRouter } from 'react-router-dom';
+
+const SideBar = ({ opened, toggleDrawer, history }) => {
+  return (
+    <Drawer anchor="left" open={opened} onClose={toggleDrawer}>
+      <List>
+        <ListItem>
+          <Box my={2}>
+            <Typography variant="h4">Pet Buddy Online</Typography>
+          </Box>
+        </ListItem>
+        <Divider />
+        <ListItem
+          button
+          onClick={() => {
+            history.push('/');
+          }}
+        >
+          <ListItemIcon>
+            <HomeIcon />
+          </ListItemIcon>
+          <Typography>Home</Typography>
+        </ListItem>
+        <ListItem
+          button
+          onClick={() => {
+            history.push('/itemReg');
+          }}
+        >
+          <ListItemIcon>
+            <AddBoxIcon />
+          </ListItemIcon>
+          <Typography>Item Registration</Typography>
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+};
+
+export const SideBarWithRouter = withRouter(SideBar);
