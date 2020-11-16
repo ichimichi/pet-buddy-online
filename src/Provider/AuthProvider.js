45228ab6e84efd7b2f1be627e80f7a4fd07f198e
Laribok Syiemlieh <ichimichilaribok@gmail.com>
2020-11-16 14:33:28 +0530

    added path and domain to cookies remove call

--- src/Provider/AuthProvider.js
+++ src/Provider/AuthProvider.js
@@ -4,9 +4,9 @@ export const logout = () => {
   const cookies = new Cookies();
 
   return new Promise((resolve, reject) => {
-    cookies.remove('payload');
-    cookies.remove('XSRF-TOKEN');
-    cookies.remove('_csrf');
+    cookies.remove('payload', { path: '/', domain: '127.0.0.1' });
+    cookies.remove('XSRF-TOKEN', { path: '/', domain: '127.0.0.1' });
+    cookies.remove('_csrf', { path: '/', domain: '127.0.0.1' });
     resolve();
   });
 };
