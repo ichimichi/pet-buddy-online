b7bfee50b55b01b8bc6c4291c5abb8a6ffe9b7c2
Laribok Syiemlieh <ichimichilaribok@gmail.com>
2020-11-17 21:49:29 +0530

    user name appears in right of navbar

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -9,20 +9,45 @@ import {
   Menu as MenuIcon,
   ExitToApp as ExitToAppIcon,
 } from '@material-ui/icons';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { logout } from '../../Provider/AuthProvider';
 import { SideBarWithRouter } from './SideBar';
 import { useStyles } from './useStyles';
 import clsx from 'clsx';
+import { useAppState } from '../../Provider/AppProvider';
+import Axios from 'axios';
 
 export const NavBar = ({ history }) => {
   const classes = useStyles();
   const [opened, setOpened] = useState(false);
-
+  const { apis } = useAppState();
+  const [fetched, setFetched] = useState(false);
+  const [user, setUser] = useState(null);
   const handleDrawerToggle = () => {
     setOpened(!opened);
   };
+
+  const getProfile = async () => {
+    const options = {
+      method: 'GET',
+      headers: { 'Content-Type': 'application/json' },
+      withCredentials: true,
+      url: apis.user,
+    };
+    try {
+      const { data } = await Axios(options);
+      console.log('fetched profile', data);
+      setUser(data);
+      setFetched(true);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+  }, []);
 
   return (
     <>
@@ -46,6 +71,9 @@ export const NavBar = ({ history }) => {
             Pet Buddy Online
           </Typography>
           <div className={classes.grow} />
+          <Typography variant="h7" color="inherit" noWrap>
+            {fetched && `${user.firstName} ${user.lastName}`}
+          </Typography>
           <Tooltip arrow title="Logout">
             <IconButton
               color="inherit"
